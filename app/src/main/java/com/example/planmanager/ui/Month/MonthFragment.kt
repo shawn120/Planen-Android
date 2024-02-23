bad24c6ed5896550670e4c4ea02fc0a834c56e8e
Yun Hsuan Chan <149023972+YShuan-Chan@users.noreply.github.com>
2024-02-23 12:21:48 -0800

    23 Feb. 2024 - Rename the file

--- app/src/main/java/com/example/planmanager/ui/Month/MonthFragment.kt
+++ app/src/main/java/com/example/planmanager/ui/Month/MonthFragment.kt
@@ -1,4 +1,4 @@
-package com.example.planmanager.ui.dashboard
+package com.example.planmanager.ui.Month
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.planmanager.databinding.FragmentDashboardBinding
+import com.example.planmanager.databinding.FragmentMonthBinding
 
-class DashboardFragment : Fragment() {
+class MonthFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentMonthBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val monthViewModel =
+            ViewModelProvider(this).get(MonthViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentMonthBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textMonth
+        monthViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/planmanager/ui/Month/MonthViewModel.kt
+++ app/src/main/java/com/example/planmanager/ui/Month/MonthViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.planmanager.ui.notifications
+package com.example.planmanager.ui.Month
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class MonthViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Month Fragment"
     }
     val text: LiveData<String> = _text
 }

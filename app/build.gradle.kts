c08257565eb6ed4c988ad3b5f35bababa1343fe8
Adoyle210 <40745653+Adoyle210@users.noreply.github.com>
2024-03-01 12:44:39 -0800

    Added login implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.credentials:credentials:1.3.0-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1"
 }

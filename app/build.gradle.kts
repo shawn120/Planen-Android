c90ecc07941119fbc86fe8adb90085d3607defc0
Adoyle210 <40745653+Adoyle210@users.noreply.github.com>
2024-03-08 09:50:26 -0800

    added missing ')'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,5 @@ dependencies {
     implementation("androidx.credentials:credentials:1.3.0-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1"
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }

879800d55ce330a752b4867a95defd666d708fcf
sabamoallem <73094543+sabamoallem@users.noreply.github.com>
2024-03-08 13:57:42 -0800

    quick update, added something twice

--- app/src/main/java/com/example/planmanager/ui/Month/MonthFragment.kt
+++ app/src/main/java/com/example/planmanager/ui/Month/MonthFragment.kt
@@ -3,7 +3,6 @@ package com.example.planmanager.ui.Month
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.view.ViewGroup
 import android.widget.TextView

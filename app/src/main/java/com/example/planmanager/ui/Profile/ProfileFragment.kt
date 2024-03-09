8dd7aa4b28466cc26275118479058c0d59ab88a7
Adoyle210 <40745653+Adoyle210@users.noreply.github.com>
2024-03-08 20:38:55 -0800

    curr

--- app/src/main/java/com/example/planmanager/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/planmanager/ui/Profile/ProfileFragment.kt
@@ -8,8 +8,22 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.planmanager.databinding.FragmentProfileBinding
+import android.content.Intent
+import android.widget.Button
+import com.example.planmanager.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class ProfileFragment : Fragment() {
+
+    // added for google sign in
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var gsc: GoogleSignInClient
+    private lateinit var name: TextView
+    private lateinit var email: TextView
+    private lateinit var signOutBtn: Button
+    // ends here
 
     private var _binding: FragmentProfileBinding? = null
 
@@ -32,11 +46,48 @@ class ProfileFragment : Fragment() {
         profileViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
-        return root
+
+        //added
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        name = view.findViewById(R.id.name)
+        email = view.findViewById(R.id.email)
+        signOutBtn = view.findViewById(R.id.signout)
+
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        gsc = GoogleSignIn.getClient(requireContext(), gso)
+
+        val acct = GoogleSignIn.getLastSignedInAccount(requireContext())
+        if (acct != null) {
+            val personName = acct.displayName
+            val personEmail = acct.email
+            name.text = personName
+            email.text = personEmail
+        }
+
+        signOutBtn.setOnClickListener {
+            signOut()
+        }
+
+        return view
+        //return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    //Added sign out functionality
+    private fun signOut() {
+        gsc.signOut().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                startActivity(Intent(requireContext(), ProfileFragment::class.java))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/planmanager/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/planmanager/ui/login/LoginFragment.kt
@@ -1,34 +1,96 @@
 package com.example.planmanager.ui.login
 
 import android.os.Bundle
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.planmanager.R
-import com.google.android.material.button.MaterialButton
-import android.widget.Button
-import android.widget.Toast
+import com.example.planmanager.ui.Profile.ProfileFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.example.planmanager.ui.Today.TodayFragment
 
-class LoginFragment : Fragment(){
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        val username: TextView = view.findViewById(R.id.username)
-        val password: TextView = view.findViewById(R.id.password)
+class LoginFragment : Fragment() {
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var gsc: GoogleSignInClient
+    private lateinit var googleBtn: ImageView
 
-        val loginBtn: MaterialButton = view.findViewById(R.id.loginbtn)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
 
-        loginBtn.setOnClickListener {
-            if (username.text.toString() == "admin" && password.text.toString() == "admin") {
-                // correct
-                Toast.makeText(requireContext(), "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
-            } else {
-                // incorrect
-                Toast.makeText(requireContext(), "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show()
+        googleBtn = view.findViewById(R.id.google_btn)
+
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        gsc = GoogleSignIn.getClient(requireContext(), gso)
+
+        val acct = GoogleSignIn.getLastSignedInAccount(requireContext())
+        if (acct != null) {
+            navigateToStart()
+        }
+
+        googleBtn.setOnClickListener {
+            signIn()
+        }
+
+        return view
+
+    //        val username: TextView = view.findViewById(R.id.username)
+    //        val password: TextView = view.findViewById(R.id.password)
+    //
+    //        val loginBtn: MaterialButton = view.findViewById(R.id.loginbtn)
+    //
+    //        loginBtn.setOnClickListener {
+    //            if (username.text.toString() == "admin" && password.text.toString() == "admin") {
+    //                // correct
+    //                Toast.makeText(requireContext(), "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+    //            } else {
+    //                // incorrect
+    //                Toast.makeText(requireContext(), "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show()
+    //            }
+    //        }
+
+    }
+
+    private fun signIn() {
+        val signInIntent = gsc.signInIntent
+        startActivityForResult(signInIntent, 1000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                task.getResult(ApiException::class.java)
+                navigateToStart()
+            } catch (e: ApiException) {
+                Toast.makeText(requireContext(). applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    private fun navigateToStart() {
+        val intent = Intent(requireContext(), TodayFragment::class.java)
+        startActivity(intent)
+    }
+
+
 }
+
+

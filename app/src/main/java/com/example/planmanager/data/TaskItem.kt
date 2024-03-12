e2c688a08b82da2fd0ab5b0c922c9afe9de02026
shawn120 <931908356@qq.com>
2024-03-12 16:00:35 -0700

    dependencies

--- app/src/main/java/com/example/planmanager/data/TaskItem.kt
+++ app/src/main/java/com/example/planmanager/data/TaskItem.kt
@@ -1,8 +1,12 @@
 package com.example.planmanager.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.planmanager.util.TaskType
 
+@Entity
 data class TaskItem(
+    @PrimaryKey val taskId: Int,
     val taskType: TaskType,
     var deadline: Deadline? = null,
     var todo: ToDoItem? = null,

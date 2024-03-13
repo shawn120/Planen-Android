7bdb6d484e5a594d07f0bf9315367dfcf564e3eb
shawn120 <931908356@qq.com>
2024-03-12 17:51:21 -0700

    implement Room, not work yet, will go back after fixing data type

--- app/src/main/java/com/example/planmanager/data/TaskItem.kt
+++ app/src/main/java/com/example/planmanager/data/TaskItem.kt
@@ -3,14 +3,18 @@ package com.example.planmanager.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.planmanager.util.TaskType
+import java.io.Serializable
 import java.util.UUID
 
 @Entity
 data class TaskItem(
-    @PrimaryKey val id: String = UUID.randomUUID().toString(),
-    val taskType: TaskType,
+    @PrimaryKey var id: String = UUID.randomUUID().toString(),
+    @Transient
+    var taskType: TaskType,
+    @Transient
     var deadline: Deadline? = null,
-
+    @Transient
     var todo: ToDoItem? = null,
+    @Transient
     var schedule: ScheduleItem? = null,
 )

31975a6aff89f932564b4e9497f25cd7f9576f87
Adoyle210 <40745653+Adoyle210@users.noreply.github.com>
2024-03-17 00:02:51 -0700

    Get Events from google calendar

---
+++ app/src/main/java/com/example/planmanager/util/CoroutinesExtension.kt
@@ -0,0 +1,21 @@
+package com.example.planmanager.util
+
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+fun <R> CoroutineScope.executeAsyncTask(
+    onStart: () -> Unit,
+    doInBackground: () -> R,
+    onPostExecute: (R) -> Unit,
+    onCancelled: () -> Unit
+) = launch {
+    onStart()
+    val result = withContext(Dispatchers.IO) {
+        doInBackground() // runs in background thread without blocking the Main Thread
+    }
+    onPostExecute(result) // runs in Main Thread
+    onCancelled()
+}

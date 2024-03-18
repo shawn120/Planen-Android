8836c091e7fa5a959b948f537af0e499abecac14
shawn120 <931908356@qq.com>
2024-03-18 15:51:57 -0700

    when todo pass due but not finished, still show, when deadline pass due, won't show

--- app/src/main/java/com/example/planmanager/data/TaskItemDao.kt
+++ app/src/main/java/com/example/planmanager/data/TaskItemDao.kt
@@ -22,15 +22,15 @@ interface TaskItemDao {
     fun getAllLocalTasks() : Flow<MutableList<TaskItem>?>
 
     @Query("SELECT * FROM TaskItem WHERE " +
-            "(isDeadline = 1 AND startDateDeadline <= date('now','localtime'))" +
-            "OR (isToDo = 1 AND dateToDo = date('now','localtime')) " +
+            "(isDeadline = 1 AND startDateDeadline <= date('now','localtime') AND dateDeadline >= date('now','localtime'))" +
+            "OR (isToDo = 1 AND dateToDo <= date('now','localtime')) " +
             "OR (isSchedule = 1 AND dateSchedule = date('now','localtime'))"+
             "ORDER BY universalDateUsingStartDate DESC, timeCreated DESC")
     fun getAllLocalTasksToday() : Flow<MutableList<TaskItem>?>
 
     @Query("SELECT * FROM TaskItem WHERE " +
-            "(isDeadline = 1 AND startDateDeadline <= :date)" +
-            "OR (isToDo = 1 AND dateToDo = :date) " +
+            "(isDeadline = 1 AND startDateDeadline <= :date AND dateDeadline >= :date)" +
+            "OR (isToDo = 1 AND dateToDo <= :date) " +
             "OR (isSchedule = 1 AND dateSchedule = :date)"+
             "ORDER BY universalDateUsingStartDate DESC, timeCreated DESC")
     fun getAllLocalTasksOnDate(date: String) : Flow<MutableList<TaskItem>?>
